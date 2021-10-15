f2a96f1e5af735cc03b3505ea58b0a1b6df3b929
desiree <desireegarcia@mini.local>
2021-10-15 10:52:17 -0600

    added html to app.js, created home and form, added router and link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, Route, Switch } from 'react-router-dom'
+import Home from './Home';
 
 const App = () => {
   return (
     <>
+    
+      <header>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/Form">Order</Link>
+        
+        </div>
+        </header>
+      
+      <Home></Home>
     </>
   );
 };
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Home () {
+
+    return (
+        <div>
+            <h2>Your favorite food, delivered while coding!</h2>
+            <img></img>
+            <button>Pizza?</button>
+        </div>
+    )
+}
